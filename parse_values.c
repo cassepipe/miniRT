476c7e091d71b5fdc02aed0e96d5dddb4227d9a3
cassepipe <cassepipe@ymail.com>
2021-04-15 16:12:37 +0200

    Checks color range

--- parse_values.c
+++ parse_values.c
@@ -111,6 +111,13 @@ t_color	parse_color(char **input)
 	color.green = parse_int(input);
 	color.blue = parse_int(input);
 
+	if (	color.red < 0
+		||	color.red > 255
+		||	color.green < 0
+		||	color.green > 255
+		||	color.blue < 0
+		||	color.blue > 255)
+		die ("Error color not in range");
 	return (color);
 }
 
